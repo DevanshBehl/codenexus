006e88f60785a164f18a54214e1b12ccfb6e13cd
Devansh Behl <xxjm5q72pb@privaterelay.appleid.com>
2026-03-12 02:33:00 +0530

    deployment

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -2,5 +2,6 @@ import dotenv from "dotenv"
 dotenv.config();
 export const ENV ={
     port : process.env.PORT,
-    database : process.env.DATABASE_URL
+    database : process.env.DATABASE_URL,
+    NODE_ENV : process.env.NODE_ENV
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,27 @@
 import express from "express";
 import { ENV } from "./lib/env";
+import path from "path";
+
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        "message":"server running successfully"
-    })
-})
-app.listen(ENV.port)
+app.get("/book", (req, res) => {
+  res.status(200).json({
+    message: "server running successfully"
+  });
+});
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  // React fallback
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+
+}
+
+app.listen(ENV.port, () => {
+  console.log(`Server running on port ${ENV.port}`);
+});
